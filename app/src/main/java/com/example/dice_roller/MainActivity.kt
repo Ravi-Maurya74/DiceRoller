4a88e4c0941b6d540d489d99c703ca89095c01a3
Beingslayer74 <lcs2020043@iiitl.ac.in>
2021-02-15 13:15:46 +0530

    DiceRoller

--- app/src/main/java/com/example/dice_roller/MainActivity.kt
+++ app/src/main/java/com/example/dice_roller/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.dice_roller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val Button=findViewById<Button>(R.id.button2)
+        val Dice=findViewById<ImageView>(R.id.imageView)
+        Dice.setImageResource(R.drawable.dice_1)
+        Button.setOnClickListener {
+            var num= (1..6).random()
+            when(num){
+                1->Dice.setImageResource((R.drawable.dice_1))
+                2->Dice.setImageResource((R.drawable.dice_2))
+                3->Dice.setImageResource((R.drawable.dice_3))
+                4->Dice.setImageResource((R.drawable.dice_4))
+                5->Dice.setImageResource((R.drawable.dice_5))
+                6->Dice.setImageResource((R.drawable.dice_6))
+            }
+        }
     }
 }
